59767c94d080f16eddc16099f9c7569ff109ce6f
aunglynn550 <aunglynn550@gmail.com>
2022-12-28 00:00:44 +0630

    mask-img-mdf

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,17 @@
 // console.log('Client side javascript file is loaded!')
 
-// const resetButton = document.getElementsByClassName(".reset-animation");
-
-
-// resetButton.addEventListener("click", () => {
-//   maskedImage.classList.remove("mask-animation");
-//   setTimeout(() => maskedImage.classList.add("mask-animation"), 100);
-// });
-
 function myfun(){
-  var maskedImage = document.querySelector('.color-image');
-  maskedImage.classList.toggle("mask-animation");
-  setTimeout(() => maskedImage.classList.add("mask-animation"), 100); 
+var resetButton = document.querySelector(".reset-animation");
+const maskedImage = document.querySelector(".color-image");
+
+resetButton.addEventListener("click", () => {
+  maskedImage.classList.remove("mask-animation");
+  setTimeout(() => maskedImage.classList.add("mask-animation"), 100);
+});
 }
+
+// function myfun(){
+//   var maskedImage = document.querySelector('.color-image');
+//   maskedImage.classList.toggle("mask-animation");
+//   setTimeout(() => maskedImage.classList.add("mask-animation"), 100); 
+// }
